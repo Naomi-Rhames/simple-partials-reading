b2cc90a39d661cf2e131a644041a4cf7cf377df8
Naomi-Rhames <naomirhames2020@gmail.com>
2021-06-25 00:46:46 -0500

    commit tree fe5cf8c4ddb9f1981355e2065118c65ad73ad899

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
+ 
   def show
     @post = Post.find(params[:id])
-  end
-
+    @author = @post.author
+   end 
+   
   def new
     @post = Post.new
   end
